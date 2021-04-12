309f03f82b1dc705b1924fa0efd11ec5899a563d
fasbrothers <nigmatovsanjar00@gmail.com>
2021-04-12 23:22:16 +0500

    made changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,28 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Mail from './Mail';
+import EmailList from './EmailList'
 
 
 function App() {
   return (
     <Router>
-       <div className="App">
-          <Header />
-          <Sidebar />
-      </div>
+        <div className="App">
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              
+              <Switch>
+                <Route path='/mail'>
+                  <Mail />
+                </Route>
+                <Route path='/'>
+                  <EmailList />
+                </Route>
+              </Switch>
+            </div>
+        </div>
     </Router>
   );
 }
---
+++ src/EmailList.js
@@ -0,0 +1,51 @@
+import { Checkbox, IconButton } from '@material-ui/core'
+import React from 'react'
+import './EmailList.css'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+
+function EmailList() {
+    return (
+        <div className='emailList'>
+            <div className="emailList__settings">
+                <div className="emailList_settingsLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className="emailList_settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+                <div className="emailList_sections">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EmailList
