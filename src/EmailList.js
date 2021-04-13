5c82a8512acb69246b96319ce2ec5a6b9c586f41
fasbrothers <nigmatovsanjar00@gmail.com>
2021-04-13 13:20:01 +0500

    made changes

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,51 +1,69 @@
-import { Checkbox, IconButton } from '@material-ui/core'
-import React from 'react'
-import './EmailList.css'
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import RedoIcon from '@material-ui/icons/Redo';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
-import SettingsIcon from '@material-ui/icons/Settings';
-
+import { Checkbox, IconButton } from "@material-ui/core";
+import React from "react";
+import "./EmailList.css";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import InboxIcon from "@material-ui/icons/Inbox";
+import PeopleIcon from "@material-ui/icons/People";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Section from "./Section";
+import EmailRow from "./EmailRow";
 
 function EmailList() {
-    return (
-        <div className='emailList'>
-            <div className="emailList__settings">
-                <div className="emailList_settingsLeft">
-                    <Checkbox />
-                    <IconButton>
-                        <ArrowDropDownIcon />
-                    </IconButton>
-                    <IconButton>
-                        <RedoIcon />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVertIcon />
-                    </IconButton>
-                </div>
-                <div className="emailList_settingsRight">
-                    <IconButton>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                    <IconButton>
-                        <ChevronRightIcon />
-                    </IconButton>
-                    <IconButton>
-                        <KeyboardHideIcon />
-                    </IconButton>
-                    <IconButton>
-                        <SettingsIcon />
-                    </IconButton>
-                </div>
-                <div className="emailList_sections">
-
-                </div>
-            </div>
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList_settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+        <div className="emailList_settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        <Section title="Primary" color="red" selected Icon={InboxIcon} />
+        <Section Icon={PeopleIcon} title="Social" color="#1a73e8" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+        <EmailRow
+        title="Youtube"
+        subject="What is up guys"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio autem, esse explicabo repellat aspernatur."
+        time='10pm' />
+        <EmailRow
+        title="Youtube"
+        subject="What is up guys"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio autem."
+        time='10pm' />
+      </div>
+    </div>
+  );
 }
 
-export default EmailList
+export default EmailList;
---
+++ src/EmailRow.js
@@ -0,0 +1,31 @@
+import { IconButton } from "@material-ui/core";
+import React from "react";
+import "./EmailRow.css";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+import Checkbox from "@material-ui/core/Checkbox";
+
+function EmailRow({ title, id, description, subject, time }) {
+  return (
+    <div className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+      <h3 className="emailRow__title">{title}</h3>
+      <div className="emailRow__message">
+        <h4>{subject}{" "}
+        <span className='emailRow__description'>-{description}</span>
+        </h4>
+      </div>
+      <p className="emailRow__time">{time}</p>
+    </div>
+  );
+}
+
+export default EmailRow;
