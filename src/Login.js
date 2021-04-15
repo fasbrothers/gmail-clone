e6beaeb0e02d2d19e88d83f371b760b45af05272
fasbrothers <nigmatovsanjar00@gmail.com>
2021-04-15 14:53:58 +0500

    created Login page

---
+++ src/Login.js
@@ -0,0 +1,40 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "./firebase";
+import { login } from "./features/userSlice";
+import { useDispatch } from "react-redux";
+
+function Login() {
+  const dispatch = useDispatch();
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://i.pinimg.com/originals/d6/9f/9f/d69f9f7aa5fd9674ae2db2faebe04478.png"
+          alt=""
+        />
+        <Button variant="contained" color="primary" onClick={signIn}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
